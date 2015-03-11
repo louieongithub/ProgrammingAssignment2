1d908bdc8008385939883b89c981cff48b367ca1
louieongithub <louie.sakellaris@gmail.com>
2015-03-11 19:46:53 +1100

    changes to programming assignment2
    changes to programming assignment2
    
    Please enter the commit message for your changes. Lines starting
    "added assignment2""added assignment2"

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,46 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The following caches the inverse of a matrix  
 
-## Write a short comment describing this function
-
+## 
 makeCacheMatrix <- function(x = matrix()) {
-
+    i <- NULL  
+#initialises object i to NULL in the defining environment.
+    set <- function(y) {#sets the value of the matrix
+        x <<- y     
+# sets  variable x to the value of inputed matrix in the calling environment.
+        i <<- NULL 
+#initialises variable i to NULL in the calling environment.
+    }
+    get <- function() x  
+#gets the value of the matrix
+    setinverse <- function(inverse) i <<- inverse  #sets the value of the inverse to i in the calling environment.
+    getinverse <- function() i 
+#gets the value of the inverse matrix from the defining environment.
+    list(set = set, get = get,  
+#lists the defining steps/functions
+         setinverse = setinverse,
+         getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## This function checks for a cached matrix inverse and if one does not exist it creates one. 
+
+cacheSolve <- function(x, ...) {cacheSolve <- function(x, ...) {
+    i <- x$getinverse() 
+#the value the inverse (from the defining environment) is assigned to i
+    if(!is.null(i)) { 
+#checks for cached inverse and if found 
+        message("getting cached data") 
+#returns message and cached inverse.
+        return(i)
+    }
+    data <- x$get() 
+# value of the matrix (from defining environment) is assigned to data
+    i <- solve(data, ...) 
+#value of inverse is calculated and assigned to i
+    x$setinverse(i) 
+#value of the inverse is set as cached
+    i                     
+#inverse is returned
 }
+
